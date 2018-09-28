157f0a829a47434c2f9af8d70e6701c24dec49d0
Omar Khatib <omar@omarkhatib.co>
2018-09-28 23:48:19 +0200

    Update index.js

--- providers/NextProvider/index.js
+++ providers/NextProvider/index.js
@@ -1,7 +1,7 @@
 /**
  * adonis-nextjs-provider
  *
- * (c) Omar Khatib <omar@omarkhatib.co.>
+ * (c) Omar Khatib
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
